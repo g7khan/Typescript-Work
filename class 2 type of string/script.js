17d544fc5a1fb254d2d1f9727b1c24a57a62d967
g7khan <owaissattar296@gmail.com>
2024-07-24 23:55:36 +0500

    Typescript

---
+++ class 2 type of string/script.js
@@ -0,0 +1,82 @@
+"use strict";
+// console.log("hello");
+// in this case that this console should be output is etc beacuse this case has written value string and number
+// let naming: string | number = "owais";
+// naming = "owais";
+// console.log(naming);
+// ----------------------------
+// this type of case that written multiple value such as boolean, string, number......
+// type Mixed = string | number | boolean;
+// let h: Mixed = "a";
+// let o: Mixed = 4;
+// let c: Mixed = "e";
+// let u: Mixed = true;
+// console.log(h, o, c, u);
+// type Mixed = string | boolean | number; 
+// let a: Mixed = "owais";
+// let b: Mixed = 5566;
+// let c: Mixed = true;
+// let d: Mixed = "khan";
+// let e: Mixed = 3344;
+// let f: Mixed = 5566;
+// let g: Mixed = "owais";
+// let h: Mixed = 5566;
+// let i: Mixed = true;
+// let j: Mixed = 5566;
+// let k: Mixed = "owais";
+// let l: Mixed = 5566;
+// let m: Mixed = "owais";
+// let n: Mixed = 5566;
+// console.log(a, b, c, d, e, f, g, h, i, j, k, l, m, n);
+// in this case any type of typescript that value assigned of last declared variable that he should be console the last value
+// let user: any;
+// user = "owais";
+// user = 6;
+// user = true;
+// console.log(user);
+// object of typescript
+// type Car = {
+//     name: string,
+//     model: number,
+//     varient: string,
+//     isAutomatic: boolean,
+//     features: string[],
+//     launchedYear?: number,
+//     calculateAvg?: (fuelQty: number, distance: number) => void
+// }
+// let car: Car = {
+//     name: 'Mehran',
+//     model: 2009,
+//     varient: 'VXR',
+//     isAutomatic: false,
+//     features: ["Air Bags", "Air Conditioned"],
+//     calculateAvg: (fuelQty, distance) => {
+//     }
+// }
+// car.launchedYear = 2000;
+// console.log(car);
+// type student = {
+//     name: string;
+//     Fathername: string;
+//     Rollno: number;
+//     Insitute: string;
+//     Age: number;
+//     Gender: string;
+//     Department: string;
+//     Bachelordegree: string;
+//     Subject: string[];
+//     Hepassedtest: boolean;
+// }
+// let student = {
+//     name: 'Owais',
+//     Fathername: 'Abdul sattar',
+//     Rollno: 331080,
+//     Institute: 'Benazir Butto shaheed university lyari',
+//     Age: 19,
+//     Gender: 'Male',
+//     Department: 'Computer Science',
+//     Bachelordegree: 'BSCS',
+//     Subject: ["ICT", "Programming fundamental", "English", "Applied physics", "Math",],
+//     Hepassedtest: true,
+// }
+// console.log(student);
